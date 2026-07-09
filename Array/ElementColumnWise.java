7789aefae90ba25a8b501df0d5b4a66ed5db7066
Kishan4237 <kishankumar4237068@gmail.com>
2026-07-09 12:17:59 +0530

    ElementColumnWise

---
+++ Array/ElementColumnWise.java
@@ -0,0 +1,8 @@
+public class ElementColumnWise {
+    public static void main(String[] args) {
+        int [][] arr = {{2,4,3,4},{5,9,7,5},{4,5,3,2}};
+        for(int i =0;i<arr[0].length;i++){
+            System.out.println(arr[0][i]+" ");
+        }
+    }
+}
